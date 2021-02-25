87a7fde2a7b67532096f3e02e0863cb14bd57e60
Victor Thury Vieira Hauache <victor.hauache@linx3.com>
2021-02-25 10:54:41 -0400

    end of 4th class - tests and other tables

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { SendMailController } from './controllers/SendMailController';
 import { SurveyController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController'
 
@@ -7,9 +8,13 @@ const router = Router();
 const userController = new UserController();
 const surveyController = new SurveyController();
 
-router.post("/users", userController.create)
+const sendMailController = new SendMailController();
 
-router.post("/surveys", surveyController.create)
-router.get("/surveys", surveyController.show)
+router.post("/users", userController.create);
+
+router.post("/surveys", surveyController.create);
+router.get("/surveys", surveyController.show);
+
+router.post("/sendMail", sendMailController.execute);
 
 export { router };
